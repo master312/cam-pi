8ff8c63814b6e6484cc196046f1d8c12ae1fc1f6
master312 <sythinian@gmail.com>
2023-06-17 02:13:10 +0200

    Added basic device fetching.

--- stream_ops.js
+++ stream_ops.js
@@ -4,14 +4,20 @@ const { exec } = require('child_process');
 
 const router = express.Router();
 
+// Devices to be ignored when retreiving list of available devices using v4l2-ctl
+const ignoredDevices = ['bcm2835-codec-decode', 'bcm2835-isp', 'rpivid'];
 
-ffmpegDefaultArgs = '-c:v h264_v4l2m2m -b:v 5M -pix_fmt yuv420p'
-ffmpegRtmpRoot = 'rtmp://localhost:1935/live/'
+const ffmpegDefaultArgs = '-c:v h264_v4l2m2m -b:v 5M -pix_fmt yuv420p'
+
+// REMAINDER: Always keep '/' on the end of this path!
+const ffmpegRtmpRoot = 'rtmp://localhost:1935/live/'
+
 activeStreams = []
 
 
-// Device model:
+// Device model example:
 // {
+//     name = 'Nikon USB CAM'
 //     path = '/dev/video0'
 //     streamName = 'device1'
 //     ffmpegArgs = '-c:v h264_v4l2m2m -b:v 5M -pix_fmt yuv420p'
@@ -20,11 +26,12 @@ activeStreams = []
 // }
 
 function generateStreamCmd(deviceInfo) {
-    return 'ffmpeg -re -i ' + deviceInfo.path + ' ' + deviceInfo.ffmpegArgs + ' -f flv ' + ffmpegRtmpRoot + '/' + deviceInfo.streamName
+    return 'ffmpeg -re -i ' + deviceInfo.path + ' ' + deviceInfo.ffmpegArgs + ' -f flv ' + ffmpegRtmpRoot + deviceInfo.streamName
 }
 
 function generateDeviceInfo(devicePath) {
     model = {
+        name: 'todo',
         path: devicePath,
         streamName: devicePath.replace('/dev/', ''),
         ffmpegArgs: ffmpegDefaultArgs,
@@ -35,22 +42,47 @@ function generateDeviceInfo(devicePath) {
     return model;
 }
 
+function extractVideoDevices(deviceString) {
+    // Matches /dev/video followed by one or more digits
+    const regex = /\/dev\/video\d+/g;
+    const matches = deviceString.match(regex);
+    return matches;
+}
 
 // Returns all camera devices connected to the machine
 router.get('/devices', async (req, res) => {
-    fs.readdir('/dev/', (err, files) => {
-        if (err) {
-            res.status(500).send(err);
-            return;
+    exec('v4l2-ctl --list-devices', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).json({ error: 'An error occurred while getting the video capture devices.' });
         }
-
-        rawPaths = files.filter(file => file.startsWith('video'));
+    
+        // split devices by double newline
+        const devices = stdout.split('\n\n');
+    
+        const videoCaptureDevices = devices.filter(device => {
+            // check if this device is not one of the ignored devices
+            return !ignoredDevices.some(ignoredDevice => device.includes(ignoredDevice));
+        });
+    
+        extractedPaths = []
+        videoCaptureDevices.forEach(item => {
+            value = extractVideoDevices(item)
+            if (value != null) {
+                value.forEach(element => {
+                    extractedPaths.push(element);
+                });
+            }
+        });
+        
         videoDevices = []
-        rawPaths.forEach(element => {
+        extractedPaths.forEach(element => {
+
             videoDevices.push(generateDeviceInfo(element));
         });
 
-        res.json(videoDevices);
+        console.log(videoDevices);
+        return res.json(videoDevices);
     });
 });
 
